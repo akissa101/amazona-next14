48fb622d9060c86e0d3013cf2562645b7f519a50
Abdikarim <kareemorbit@gmail.com>
2024-05-22 04:36:11 +0300

    little update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,6 @@ export default async function Home() {
   const latestProducts = await getLatestProducts();
   return (
     <div className="space-y-8">
-      <h2 className="h2-bold">Latest Products</h2>
       <ProductList title="Newest Arrivals" data={latestProducts} />
     </div>
   );
